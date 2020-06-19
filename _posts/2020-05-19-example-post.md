52a28847ab94817ae505af54272948ea57de5b1c
Dr Kim Foale <kim@gfsc.studio>
2020-06-19 14:37:22 +0100

    Initial commit

---
+++ _posts/2020-05-19-example-post.md
@@ -0,0 +1,10 @@
+---
+layout: blog
+title: Blog post title
+description: Blog post description
+author: kim
+social-image-wide:
+social-image-square:
+---
+
+Blog post content.
---
+++ _site/assets/js/main.js
@@ -0,0 +1,13 @@
+(function() {
+  var hamburger = {
+    toggle: document.getElementById('js-toggle'),
+    nav: document.getElementById('js-nav'),
+    doToggle: function(e) {
+     this.toggle.classList.toggle('expanded');
+     this.nav.classList.toggle('expanded');
+    }
+  }
+
+  hamburger.toggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+}());
